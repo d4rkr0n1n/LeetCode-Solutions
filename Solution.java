3962f796f95c73515cdf6e66a3fe39190af39e91
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2023-09-28 12:34:47 +0530

    Add files via upload

---
+++ Solution.java
@@ -0,0 +1,13 @@
+package com.d4rkr0n1n;
+
+import java.util.Arrays;
+
+public class Solution {
+
+  // 88. Merge Sorted Array
+  public void merge(int[] nums1, int m, int[] nums2, int n) {
+    System.arraycopy(nums2, 0, nums1, m, n);
+    Arrays.sort(nums1);
+  }
+  
+}
