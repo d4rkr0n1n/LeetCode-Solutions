2c479bc2838bcaa0e4c2228fd5a56372a7951716
Mridul Roy <39593927+d4rkr0n1n@users.noreply.github.com>
2023-09-29 07:50:35 +0530

    Update SolutionTest.java

--- src/test/java/com/d4rkr0n1n/topinterview150/SolutionTest.java
+++ src/test/java/com/d4rkr0n1n/topinterview150/SolutionTest.java
@@ -1,6 +1,10 @@
 package com.d4rkr0n1n.topinterview150;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -53,6 +57,37 @@ public class SolutionTest {
         void test() {
             solution.merge(nums1, m, nums2, n);
             assertArrayEquals(expected, nums1);
+        }
+    }
+
+    // 27. Remove Element
+    @Nested
+    class testRemoveElement {
+
+        int nums[],val,expected;
+
+        @Test
+        void case_1() {
+            nums = new int[] { 3,2,2,3 }; 
+            val = 3; 
+            expected = 2;
+            test();
+        }
+
+        @Test
+        void case_2() {
+            nums = new int[] { 0, 1, 2, 2, 3, 0, 4, 2 }; 
+            val = 2; 
+            expected = 5;
+            test();
+        }
+
+        void test(){
+            assertEquals(expected, solution.removeElement(nums, val));
+            assertTrue(IntStream.range(0, nums.length)
+                    .filter(i -> nums[i] == val)
+                    .findFirst()
+                    .orElse(-1) >= expected);
         }
     }
 }
